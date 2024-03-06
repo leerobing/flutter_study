822d0fb36e6431e6e9daca2a11a613c89a4e341b
leerobing <105533691+leerobing@users.noreply.github.com>
2024-03-06 10:35:55 +0900

    feat: 1차 Map, Set 컬렉션 학습

--- lib/dartStudy_1.dart
+++ lib/dartStudy_1.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_study/Status.dart';
+
 void main() {
   List<String> memberList = ["제니","리사","지수","로제"];
 
@@ -7,7 +9,23 @@ void main() {
   var reduceList = memberList.reduce((value, element) => value + "," + element);
   var foldList = memberList.fold<int>(0,(value, element) => value + element.length);
 
+  Map<int,String> mamberMap = {
+    1:"해리포터",
+    2: "말포이",
+    3: "헤르미온느"
+  };
+  
+  Set<String> memberSet = new Set();
+  
+  memberSet.add("제니");
+  memberSet.add("리사");
+  memberSet.add("지수");
 
+
+  
+  print(mamberMap.keys);
+  print(mamberMap.values);
+  print(mamberMap[1]);
 
 
   print(finalList);
@@ -20,5 +38,16 @@ void main() {
 
   print(foldList);
 
+  print(memberSet.contains("제니"));
+  print(memberSet.toList());
+  
+  List<String> memberSetList = memberSet.toList();
+  memberSetList.add("제니");
+  print(memberSetList);
+  print(Set.from(memberSetList));
+
+
+  Status status = Status.approved;
+  print(status);
 
 }
